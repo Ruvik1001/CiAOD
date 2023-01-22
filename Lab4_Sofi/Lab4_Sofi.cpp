5622cc7c46236bc0e746c4907db1b55ec193360e
Rudnev Viktor <rudnevvictor2003@mail.ru>
2023-01-23 00:22:46 +0300

    Lab4 ended

---
+++ Lab4_Sofi/Lab4_Sofi.cpp
@@ -0,0 +1,25 @@
+﻿#include "Tree.h"
+
+using namespace std;
+
+int main() {
+	string postfix = inf_to_postf("(7*2-1)*(2+2)-2*4*2");
+	Node* root = construct(postfix);
+
+	cout << "Postfix Expression: ";
+	postorder(root);
+
+	cout << "\nInfix Expression: ";
+	inorder(root);
+
+	cout << "\nPrefix Expression: ";
+	preforder(root);
+
+	cout << "\n=================================================\n\n";
+	dump4(root);
+
+	cout << "\n=================================================\n\n";
+	cout << "Answer: " << calculate(root) << "\n";
+	cout << "Answer form prefics calculate: " << precalculate(postToPre(postfix));
+	return 0;
+}
