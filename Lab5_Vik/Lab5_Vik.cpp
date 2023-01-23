5aac7bb5e395b53ff80196478ea47b05944eff21
Rudnev Viktor <rudnevvictor2003@mail.ru>
2023-01-23 18:45:48 +0300

    Lab 5 started

---
+++ Lab5_Vik/Lab5_Vik.cpp
@@ -0,0 +1,11 @@
+﻿#include "BTS.h"
+
+int main() {
+	BTS<int, string> bt;
+	for (int i = 0; i < 11; i++)
+		bt.add_uniq(rand() % 100, "Test_" + to_string(i));
+	bt.print();
+	bt.remove(67);
+	cout << "\n\n";
+	bt.print();
+}
